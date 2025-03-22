8e91d91f40dff4854045a6083a096d658db840af
fadhlidzaki <fadhlidzaki120@gmail.com>
2025-03-22 20:32:49 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SayaTubeVideo {
+    private int id;
+    private string title;
+    private int playCount;
+    
+    public SayaTubeVideo( string title)
+    {
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+    public void IncreasePlayCount(int count)
+    {
+        if (count < 0)
+        {
+            Console.WriteLine("jumlah tidak boleh negatif");
+        }
+        this.playCount += count;
+    }
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design by Contract - Fadhli Muhammad Dzaki");
+        video1.IncreasePlayCount(10);
+        video1.PrintVideoDetails();
+    }
+}
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b16407550b35129f5ab28e0bb3700b4b1a08dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
